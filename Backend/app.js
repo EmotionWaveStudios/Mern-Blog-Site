ab71b4065d8ce1d1550dbaff8587a6a66e1eb5eb
Mzwandile <mzwandilethatojunior@gmail.com>
2023-08-25 14:19:29 +0200

    project update

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() =>
+    app.listen(process.env.PORT, () => {
+      console.log(
+        "app is listening to localhost and port number",
+        process.env.PORT
+      );
+    })
+  )
+  .catch((err) => {
+    console.log(err);
+  });
