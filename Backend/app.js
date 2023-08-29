41993bd0e83148e59c64bd5f41e721e3f97f6880
Mzwandile <mzwandilethatojunior@gmail.com>
2023-08-29 22:46:44 +0200

    project update

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,9 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import cors from "cors";
-
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import router from './route/users-route';
+import blogRouter from './route/blogs-route';
 dotenv.config();
 
 const app = express();
@@ -10,16 +11,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/user', router); // localhost:5000/api/user/
+app.use('/api/blog', blogRouter); // localhost:5000/api/blog/
+
 mongoose
   .connect(process.env.MONGO_DB)
-  .then(() => {
+  .then(() =>
     app.listen(process.env.PORT, () => {
       console.log(
-        "app is listening to localhost and port number",
+        'app is Listing to localhost and post number is',
         process.env.PORT
       );
-    });
-  })
+    })
+  )
   .catch((err) => {
     console.log(err);
   });
